9b84193b0c7092e952cdee0b971acb5b98b5c370
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-07-23 02:06:47 -0300

    Adicionando responsividade aos buttons com media query

--- src/components/BtnLinks.jsx
+++ src/components/BtnLinks.jsx
@@ -9,25 +9,25 @@ import "../styles/Button.css";
 
 const BtnLinks = () => {
   return (
-    <div className="container-btn-area">
-      <div className="content-btn-area">
+    <div className="container-btn">
+      <div className="content-btn">
         <div className="btn-area">
-          <UserOutlined />
+          <UserOutlined className="icon"/>
           <a href="#">
             <button className="btn">Sobre mim</button>
           </a>
         </div>
 
         <div className="btn-area">
-          <GithubFilled />
-          <a href="https://github.com/GabrielCarvalhouo">
+          <GithubFilled className="icon"/>
+          <a target="_blank" href="https://github.com/GabrielCarvalhouo">
             <button className="btn">github</button>
           </a>
         </div>
 
         <div className="btn-area">
-          <LinkedinOutlined/>
-          <a href="https://www.linkedin.com/in/gabriel-carvalho-a110371a1/">
+          <LinkedinOutlined className="icon"/>
+          <a target="_blank" href="https://www.linkedin.com/in/gabriel-carvalho-a110371a1/">
             <button className="btn">linkedin</button>
           </a>
         </div>
