1dc85cf98a7baf1114420c99acf3c9dbcab9d39e
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-07-27 15:50:37 -0300

    alteração da resposta dos links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,15 +18,15 @@ const Footer = () => {
           <h3>Contacts</h3>
           <ul>
             <li>
-              <a href="https://www.linkedin.com/in/gabriel-carvalho-a110371a1/">
+              <a target="_blank" href="https://www.linkedin.com/in/gabriel-carvalho-a110371a1/">
                 Linkedin
               </a>
             </li>
             <li>
-              <a href="https://github.com/GabrielCarvalhouo">Github</a>
+              <a target="_blank" href="https://github.com/GabrielCarvalhouo">Github</a>
             </li>
             <li>
-              <a href="https://www.instagram.com/cafeemjava/">Instagram</a>
+              <a target="_blank" href="https://www.instagram.com/cafeemjava/">Instagram</a>
             </li>
           </ul>
         </div>
